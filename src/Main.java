58de09848c17b1629e7078a92a666d2950b74a3a
Xeliaks <borachukbogdan@gmail.com>
2023-09-17 22:05:15 +0300

    Done.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("Welcome to my first java program!");
+        System.out.println("My name is Bogdan Borachuk.");
+        System.out.println("This program is written using IntelliJ IDEA.");
+        System.out.println("I'm excited to write this text!");
+        }
+    }
